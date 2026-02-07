1d8150ca810d61050233688432fb7b80704f80ce
Sadiya Shaikh <sadiyashaikh1699@gmail.com>
2026-02-07 13:28:14 +0530

    Add validation for echo body

--- src/controllers/appController.js
+++ src/controllers/appController.js
@@ -5,3 +5,16 @@ exports.health = (req, res) =>{
 exports.hello = (req,res) =>{
     res.send("Hello backend");
 }
+
+exports.greatUser = (req,res) =>{
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
+}
+
+exports.echo = (req,res) =>{
+    const data = req.body;
+
+    res.json({
+        received: data,
+    });
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,7 @@ const controller = require("../controllers/appController");
 router.get("/health", controller.health);
 router.get("/hello", controller.hello);
 
+router.get("/greet/:name",controller.greatUser);
+router.post("/echo",controller.echo);
+
 module.exports = router;
